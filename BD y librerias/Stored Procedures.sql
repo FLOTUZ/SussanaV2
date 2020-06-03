f1f165d946f104b9ae17ef9df4b6e777a5148f3c
Emmanuel <flotuz10@gmail.com>
2020-06-03 02:40:18 -0500

    Se agregaron más stored procedures necesarios

--- BD y librerias/Stored Procedures.sql
+++ BD y librerias/Stored Procedures.sql
@@ -25,16 +25,16 @@ begin
     where p.NC like NC;
 end;
 
-create procedure INSERT_grupo(_letra varchar(1))
+create procedure INSERT_grupo(_letra varchar(1), _tutor_iddocente int)
     -- ---------------------------------------------
 -- Se inserta un grupo
 -- @Autor: Emmanuel Esquivel Pardo
 -- ---------------------------------------------
 begin
-    insert into grupo(letra) values (_letra);
+    insert into grupo(letra, tutor_iddocente) values (_letra, _tutor_iddocente);
 end;
 
-create procedure INSERT_PERSONA(_nombre VARCHAR(45),
+create procedure INSERT_persona(_nombre VARCHAR(45),
                                 _apellidos varchar(90), _NC int)
     -- ---------------------------------------------
 -- Se inserta una persona
@@ -95,7 +95,7 @@ begin
 end;
 
 create procedure SELECT_personas()
--- ---------------------------------------------
+    -- ---------------------------------------------
 -- Se retorna todas las peronas de la tabla personas
 -- @Autor: Emmanuel Esquivel Pardo
 -- ---------------------------------------------
@@ -104,7 +104,7 @@ begin
 end;
 
 create procedure SELECT_personasByNC(_NC int)
--- ---------------------------------------------
+    -- ---------------------------------------------
 -- Se consulta persona por numero de control
 -- @Autor: Emmanuel Esquivel Pardo
 -- ---------------------------------------------
@@ -115,26 +115,231 @@ begin
     where NC like _NC;
 end;
 
-create procedure UPDATE_persona(_idPersona int ,_nombre varchar(45), _apellidos varchar(90), _NC int)
+create procedure UPDATE_persona(_idPersona int, _nombre varchar(45), _apellidos varchar(90), _NC int)
 begin
--- ---------------------------------------------
+    -- ---------------------------------------------
 -- Se actualiza datos de una persona por numero de control
 -- @Autor: Emmanuel Esquivel Pardo
 -- ---------------------------------------------
 
     UPDATE persona p
-        set p.nombre = _nombre,
-            p.apellidos = _apellidos,
-            p.NC = _NC
+    set p.nombre    = _nombre,
+        p.apellidos = _apellidos,
+        p.NC        = _NC
     where p.idPersona = _idPersona;
 end;
 
 create procedure DELETE_persona(_NC int)
--- ---------------------------------------------
+    -- ---------------------------------------------
 -- Se eliminan los datos de una persona por NC
 -- @Autor: Emmanuel Esquivel Pardo
 -- ---------------------------------------------
 
 begin
     DELETE from persona where NC = _NC;
+end;
+
+create procedure INSERT_bajaAusentismo(_fecha DATE,
+                                       _docenteReporta varchar(45),
+                                       _asignatura varchar(45),
+                                       _clave int,
+                                       _fechasInasistenciasInjustificadas json,
+                                       _posibleCausa varchar(50),
+                                       _resultadoInvestigacion varchar(255),
+                                       _idAlumno int)
+begin
+    insert into bajaausentismo(fecha,
+                               docenteReporta,
+                               asignatura,
+                               clave,
+                               fechasInasistenciasInjustificadas,
+                               posibleCausa,
+                               resultadoinvestigacion,
+                               alumno_idAlumno)
+    VALUES (_fecha,
+            _docenteReporta,
+            _asignatura,
+            _clave,
+            _fechasInasistenciasInjustificadas,
+            _posibleCausa,
+            _resultadoInvestigacion,
+            _idAlumno);
+end;
+
+create procedure INSERT_canalizacion(_fecha Date,
+                                     _idTutor int,
+                                     _idAlumno int,
+                                     _idCarrera int,
+                                     _trizteza tinyint,
+                                     _angustia tinyint,
+                                     _ansiedad tinyint,
+                                     _desesperacion tinyint,
+                                     _llanto tinyint,
+                                     _conducta tinyint,
+                                     _animo tinyint,
+                                     _exitacion tinyint,
+                                     _irritabilidad tinyint,
+                                     _drogas tinyint,
+                                     _aprendizaje tinyint,
+                                     _autoAgresiones tinyint,
+                                     _otro VARCHAR(255),
+                                     _Descripcion text(255))
+begin
+    -- ---------------------------------------------
+-- Se inserta una nuevacanalización
+-- @Autor: Emmanuel Esquivel Pardo
+-- ---------------------------------------------
+
+    insert into canalizacion
+    (fecha,
+     Tutor_idTutor,
+     Alumno_idAlumno,
+     carrera_idcarrera,
+     tristeza,
+     angustia,
+     ansiedad,
+     desesperacion,
+     llanto,
+     conducta,
+     animo,
+     exitacion,
+     irritabilidad,
+     drogas,
+     aprendizaje,
+     autoAgresiones,
+     otro,
+     Descripcion)
+    VALUES ();
+end;
+
+create procedure SELECT_tutores()
+    -- ---------------------------------------------
+-- Se consultan tutores
+-- @Autor: Emmanuel Esquivel Pardo
+-- ---------------------------------------------
+begin
+    select iddocente,
+           departamento,
+           puesto,
+           correo,
+           telefono,
+           extension,
+           persona_idPersona
+    from tutor;
+end;
+
+create procedure INSERT_tutor(_departamento varchar(45),
+                              _puesto varchar(45),
+                              _correo varchar(45),
+                              _telefono int(20),
+                              _extension int(4),
+                              _persona_idPersona int)
+    -- ---------------------------------------------
+-- Se inserta un tutor
+-- @Autor: Emmanuel Esquivel Pardo
+-- ---------------------------------------------
+begin
+    insert into tutor (departamento,
+                       puesto,
+                       correo,
+                       telefono,
+                       extension,
+                       persona_idPersona)
+    VALUES (_departamento,
+            _puesto,
+            _correo,
+            _telefono,
+            _extension,
+            _persona_idPersona);
+end;
+
+create procedure SELECT_carreras()
+    -- ---------------------------------------------
+-- Se consultan las carreras
+-- @Autor: Emmanuel Esquivel Pardo
+-- ---------------------------------------------
+begin
+    select idcarrera, nombre from carrera;
+end;
+
+create procedure INSERT_credito(_idCredito int,
+                                _folio int,
+                                _status text,
+                                _fecha date,
+                                _tipo varchar(45),
+                                _Alumno_idAlumno int)
+begin
+    -- ---------------------------------------------
+-- Se inserta credito complementario
+-- @Autor: Emmanuel Esquivel Pardo
+-- ---------------------------------------------
+    insert into credito (idCredito,
+                         folio,
+                         status,
+                         fecha,
+                         tipo,
+                         Alumno_idAlumno)
+    values (_idCredito,
+            _folio,
+            _status,
+            _fecha,
+            _tipo,
+            _Alumno_idAlumno);
+end;
+
+create procedure SELECT_grupos()
+    -- ---------------------------------------------
+-- Se consultan los grupos
+-- @Autor: Emmanuel Esquivel Pardo
+-- ---------------------------------------------
+begin
+    select idGrupo, letra, tutor_iddocente
+    from grupo;
+end;
+
+create procedure UPDATE_grupo(_idGrupo int, _letra VARCHAR(1), _idTutor int)
+    -- ---------------------------------------------
+-- Se acuatliza un grupo
+-- @Autor: Emmanuel Esquivel Pardo
+-- ---------------------------------------------
+begin
+    UPDATE grupo g
+    set g.letra           = _letra,
+        g.tutor_iddocente =_idTutor
+    where g.idGrupo = _idgrupo;
+end;
+
+create procedure DELETE_grupo(_idGrupo int)
+    -- ---------------------------------------------
+-- Se elimina un grupo
+-- @Autor: Emmanuel Esquivel Pardo
+-- ---------------------------------------------
+begin
+    DELETE from grupo where idGrupo = _idGrupo;
+end;
+
+create procedure SELECT_tutorByNC(_NC int)
+    -- ---------------------------------------------
+-- Se consulta grupo por NC
+-- @Autor: Emmanuel Esquivel Pardo
+-- ---------------------------------------------
+begin
+    select p.NC, p.nombre, p.apellidos, puesto, correo, telefono, extension
+    from tutor t
+             inner join persona p on t.persona_idPersona = p.idPersona
+    where p.NC like _NC;
+end;
+
+create procedure UPDATE_tutor(_idTutor int ,_nombre varchar(45),
+                              _apellidos varchar(90),
+                              _NC int)
+-- ---------------------------------------------
+-- Se actualiza por id
+-- @Autor: Emmanuel Esquivel Pardo
+-- ---------------------------------------------
+begin
+    UPDATE tutor t inner join persona p on t.persona_idPersona = p.idPersona
+    set p.nombre = _nombre,
+        p.apellidos = _apellidos
+        where p.NC = _NC;
 end;
--- BD y librerias/llamadas a stored procedures.sql
+++ BD y librerias/llamadas a stored procedures.sql
@@ -8,7 +8,7 @@ call DELETE_alumno(?);
 -- Tabla persona
 call SELECT_personas();
 call SELECT_personasByNC();
-call INSERT_PERSONA(?,?,?);
+call INSERT_persona(?,?,?);
 call UPDATE_persona(?,?,?,?);
 call DELETE_persona(?);
 
@@ -18,8 +18,44 @@ call SELECT_docenteByNC();
 call INSERT_docente();
 call DELETE_docente();
 
+-- Tabla tutor
+call SELECT_tutores();
+call SELECT_tutorByNC();
+call INSERT_tutor();
+call UPDATE_tutor();
+call DELETE_tutor();
 
-call INSERT_carrera(?);
-call INSERT_grupo(?);
+-- Tabla Grupo
+call SELECT_grupos();
+call SELECT_grupoById();
+call INSERT_grupo(?, ?);
+call UPDATE_grupo();
+call DELETE_grupo();
 
+-- Tabala credito
+call SELECT_creditos();
+call SELECT_creditoByFolio();
+call INSERT_credito();
+call DELETE_credito();
+call UPDATE_credito();
 
+-- Tabla carrera
+call SELECT_carreras();
+call SELECT_creditoById();
+call INSERT_carrera();
+call DELETE_credito();
+call UPDATE_credito();
+
+-- Tabla Baja Ausentismo
+call SELECT_bajasAusentismo();
+call SELECT_bajaAusentismoById();
+call INSERT_bajaAusentismo();
+call DELETE_bajaAusentismo();
+call UPDATE_bajaAusentismo();
+
+-- Tabla canalización
+call SELECT_canalizaciones();
+call SELECT_canalizacionByNC();
+call INSERT_canalizacion();
+call DELETE_canalizacion();
+call UPDATE_canalizacion();
